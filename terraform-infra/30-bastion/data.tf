379d03c4eb677e8c8dbd4607553725f77e522fb7
Naga Ajay <nagaajay@mac.myfiosgateway.com>
2026-08-01 10:36:50 -0400

    “actions”

--- terraform-infra/30-bastion/data.tf
+++ terraform-infra/30-bastion/data.tf
@@ -1,13 +1,20 @@
+data "aws_ami" "joindevops" {
+  owners = ["973714476881"]
+
+  filter {
+    name   = "name"
+    values = ["Redhat-9-DevOps-Practice*"] # Added wildcard * for safety
+  }
+  filter {
+    name   = "image-id"
+    values = ["ami-0220d79f3f480ecf5"]
+  }
+
+}
+
+data "aws_ssm_parameter" "public_subnets" {
+  name = "/${var.project}/${var.environment}/public_subnets_ids"
+}
 data "aws_ssm_parameter" "bastion" {
   name = "/${var.project}/${var.environment}/bastion_sg_id" # Replace with your SSM parameter path
 }
-data "aws_ssm_parameter" "mongodb" {
-  name = "/${var.project}/${var.environment}/mongodb_sg_id" # Replace with your SSM parameter path
-}
-data "aws_ssm_parameter" "catalogue" {
-  name = "/${var.project}/${var.environment}/catalogue_sg_id" # Replace with your SSM parameter path
-}
-data "aws_ssm_parameter" "user" {
-  name = "/${var.project}/${var.environment}/user_sg_id" # Replace with your SSM parameter path
-}
-
