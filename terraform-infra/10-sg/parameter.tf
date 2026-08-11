1e6577c663c1b26540973260a7c9298f799a1241
Naga Ajay <nagaajay@mac.myfiosgateway.com>
2026-08-11 13:25:39 -0400

    “actions”

--- terraform-infra/10-sg/parameter.tf
+++ terraform-infra/10-sg/parameter.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "sg_ids" {
   count       = length(var.sg_names)
-  name        = "/${var.project}/${var.environment}/${var.sg_names[count.index]}_sg_id"
+  name        = "/${var.project}/${var.environment}/${var.sg_names[count.index]}_sg_id" #/roboshop/dev/bastion_sg_id
   description = "The ID of the main VPC"
   type        = "String"
   value       = module.sg[count.index].sg_ids
