a961af3f16afa1fc9687abfa71950a3566c4b801
Naga Ajay <nagaajay@mac.myfiosgateway.com>
2026-08-14 13:15:28 -0400

    “actions”

--- terraform-infra/50-Backend-alb/local.tf
+++ terraform-infra/50-Backend-alb/local.tf
@@ -2,7 +2,7 @@ locals {
 
   private_subnet_list = split(",", data.aws_ssm_parameter.private_subnets.value)
 
-  backend_alb_sg_id = data.aws_ssm_parameter.mongodb.value
+  backend_alb_sg_id = data.aws_ssm_parameter.backend_alb.value
 
 
   common_tags = {
